601e3f924f0de339881b44fb2081550590fbe295
Ansh Garg <anshgarg030404@gmail.com>
2024-02-10 11:55:23 +0530

    First commit

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      animation: {
+        typewriter: "typewriter 2s steps(11) forwards"
+      },
+      keyframes: {
+        typewriter: {
+          to: {
+            left: "100%"
+          }
+        }
+      }
+    },
+  },
+  plugins: [],
+}
